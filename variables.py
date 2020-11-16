219f04bad75223840b7c0e3d8bb62df89ff6edb2
Puneet <birdipuneet@gmail.com>
2020-11-16 15:37:48 -0500

    Playing with variables.

--- variables.py
+++ variables.py
@@ -5,7 +5,24 @@ VARIABLE RULES:
  - Can have numbers but not start with one
 '''
 
-x = 1 #integer
-y = 2.5 #float
-name = 'Puneet' #string
-is_cool = True #boolean
+x = 1  # integer
+y = 2.5  # float
+name = 'Puneet'  # string
+is_cool = True  # boolean
+
+# Assigning multiple variables shorthand
+x, y, name, is_cool = (2, 3.5, 'Not-Puneet', False)
+
+print(x, y, name, is_cool)
+
+# Basic math
+a = x + y
+
+print(a)
+
+# Check type
+print(type(x))
+
+# Casting
+x = str(x)
+print(type(x))
