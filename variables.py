108a164d53ddaac2fd6e8f1897e6a4a6c625f5e4
Puneet <birdipuneet@gmail.com>
2020-11-16 15:33:28 -0500

    First commit.

---
+++ variables.py
@@ -0,0 +1,11 @@
+'''
+VARIABLE RULES:
+ - Variable names are case sensitive
+ - Must start with a letter or an underscore
+ - Can have numbers but not start with one
+'''
+
+x = 1 #integer
+y = 2.5 #float
+name = 'Puneet' #string
+is_cool = True #boolean
