c6962068d77d691fc735f1bd70118a3f06ccb385
Puneet <birdipuneet@gmail.com>
2020-11-23 12:19:53 -0500

    Working with functions.

---
+++ functions.py
@@ -0,0 +1,40 @@
+# A function is a block of code which only runs when it is called. In python we do not use parentheses or curly brackets, we use indentation with tabs or spaces.
+
+# Create function
+
+# The equal in the arguement is a default value that would be overriden by any arguement that's passed through when the function is called.
+def sayHello(name='buddy.'):
+    """
+    Prints hello and then name. This is a docstring that is meant to describe what the function does.
+    """
+    print('Hello ' + name)
+
+
+# Return value
+def getSum(num1, num2):
+    total = num1 + num2
+    return total
+
+
+numSum = getSum(2, 3)
+
+
+def addOnetoNum(num):
+    num += 1
+    return num
+
+
+num = 5
+new_num = addOnetoNum(num)
+print(new_num)
+
+
+# A lambda function is a small anonymous function.
+# A lamba function can take any number of arguments. Very similar to JS arrow functions.
+
+getSum = lambda num1, num2 : num1 + num2
+
+print(getSum(9,2))
+
+addOnetoNum = lambda num : num + 1
+print(addOnetoNum(5))
